acc7573ac51a3920f87034b4eee6808d1d2160a0
sblinn <sblinn@gac.edu>
2022-11-01 12:47:52 -0500

    Delete SecurityWebApplicationInitializer.java

--- userlogin/src/main/java/com/sblinn/userlogin/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.sblinn.userlogin;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-    
-}
