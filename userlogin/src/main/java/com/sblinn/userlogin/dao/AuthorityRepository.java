76e56b572c756d4abbc96658d5eed95a965e4747
sblinn <sblinn@gac.edu>
2022-12-18 17:40:47 -0600

    Remove unnecessary annotation

--- userlogin/src/main/java/com/sblinn/userlogin/dao/AuthorityRepository.java
+++ userlogin/src/main/java/com/sblinn/userlogin/dao/AuthorityRepository.java
@@ -4,10 +4,7 @@ package com.sblinn.userlogin.dao;
 import com.sblinn.userlogin.dto.Authority;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-
-@Repository
 public interface AuthorityRepository extends CrudRepository<Authority, String> {
     
     @Query("DELETE FROM Authority a WHERE a.authority = :authority")
